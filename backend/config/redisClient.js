282b6f7eaf2e6047623142314f5dbcdfb995c2af
Aditya Ashok Singh <adityaashoksingh7@gmail.com>
2026-06-22 23:04:20 +0530

    fixed redis server connectivity

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -2,13 +2,13 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const redisClient = createClient({
-  username: "default",
-  password: process.env.REDIS_PASS,
-  socket: {
-    host: "redis-12469.c301.ap-south-1-1.ec2.cloud.redislabs.com",
-    port: 12469,
-  },
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-14825.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: 14825
+    }
 });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
